cb6b81bb9bb4de96944dd18f6ca5ed4b43275387
Rohit Yadav <rohityadav85801@gmail.com>
2025-08-15 09:37:22 +0000

    bug : fixed bug and error

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Browse from "./components/Browse";
-import useUserStore from "./store/userStore";
 
 function App() {
-const isLoading = useUserStore((state) => state.isLoading);
   return (
     <>
    <Routes>
   <Route path="/" element={<Login />} />
   <Route path="/browse" element={<Browse />} />
 </Routes>
-
-
     </>
   );
 }
--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,7 +1,34 @@
 import type React from "react";
 import Header from "./Header";
+import axios from "axios";
+import { useEffect } from "react";
+
+import useUserStore from "../store/userStore";
+import { useNavigate } from "react-router-dom";
 
 const Browse : React.FC = () => {
+const navigate = useNavigate()
+  const {user} = useUserStore();
+
+  if(!user){
+    navigate("/");
+    return;
+  }
+
+    const fetchNowPlayingMovies = async() => {
+  const movieResponse = await axios.get("https://api.themoviedb.org/3/movie/now_playing", {
+    headers : {
+accept : "application/json",
+Authorization : `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
+  }
+})
+console.log(movieResponse.data);
+    }
+
+    useEffect(() => {
+    fetchNowPlayingMovies()
+},[])
+
     return <div
     className=" w-full h-screen"
     >
